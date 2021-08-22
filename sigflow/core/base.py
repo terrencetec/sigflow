e6c1a4366fbedcf5dca85e59e79c57a940bda2c0
lamyc <lamyc627@gmail.com>
2021-08-22 20:15:37 +0800

    layout

---
+++ sigflow/core/base.py
@@ -0,0 +1,14 @@
+from sigflow import blocks
+
+class System:
+    """A generic system class that connect blocks.
+
+    Attributes
+    ----------
+    
+    """
+    def __init__(self,):
+        """Constructor.
+
+        """
+        
