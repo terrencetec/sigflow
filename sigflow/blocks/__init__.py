9fa59c2d2a3adb1291e754fe16f30bd06f0ca691
terrencetec <terrencetec@gmail.com>
2021-10-15 15:22:30 +0900

    merge? and added LTI tests.

--- sigflow/blocks/__init__.py
+++ sigflow/blocks/__init__.py
@@ -1,5 +1,6 @@
 from .base import *
-from .filter import *
+# sigflow.blocks.filter is deprecated. See sigflow.blocks.lti.
+# from .filter import *
 from .junction import *
 from .lti import *
 from .matrix import *
---
+++ tests/blocks/test_lti.py
@@ -0,0 +1,35 @@
+import control
+import numpy as np
+
+import sigflow
+
+def test_lti():
+    """Test LTI blocks"""
+    tf = control.ss2tf(control.rss(20, 1, 1, strictly_proper=True))
+    s = control.tf("s")
+    tf_notproper = s
+    tf_unstable = 1/(s-1)
+    ## test excpetion catching
+    try:
+        sigflow.blocks.LTI(tf=1, dt=1)
+        raise
+    except TypeError:
+        pass
+    try:
+        sigflow.blocks.LTI(tf=tf_notproper, dt=1)
+        raise
+    except ValueError:
+        pass
+    try:
+        sigflow.blocks.LTI(tf=tf_unstable, dt=1)
+        raise
+    except ValueError:
+        pass
+    fs = 128
+    dt = 1/fs
+    t_final = 10
+    t = np.linspace(0, t_final, fs*t_final)
+    np.random.seed(123)
+    u = np.random.normal(0, 1, len(t))
+    sigflow_tf = sigflow.blocks.LTI(tf=tf, dt=dt)
+    #TODO How to check if the output is expected??
